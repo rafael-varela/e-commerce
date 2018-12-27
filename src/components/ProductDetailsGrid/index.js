752f31d71697dfb01bdae199f40fa580a40247c8
Rafael Varela <rafael.vrlp@gmail.com>
2018-12-27 16:45:07 -0300

    select product only when clicking on image and name

--- src/components/ProductDetailsGrid/index.js
+++ src/components/ProductDetailsGrid/index.js
@@ -3,11 +3,11 @@ import ProductForm from '../ProductForm'
 import './styles.css';
 
 const ProductDetailsGrid = ({ data, addToCart, selectProduct }) => (
-  <div className='product-details' onClick={() => selectProduct(data)}>
-    <div className='product-image'>
+  <div className='product-details'>
+    <div className='product-image' onClick={() => selectProduct(data)}>
       <img alt={data.name} src={data.picture}/>
     </div>
-    <div className='product-description'>
+    <div className='product-description' onClick={() => selectProduct(data)}>
       <p><strong>{data.name}</strong> - {data.company}</p>
     </div>
     <div className='product-price-wrapper'>
--- src/components/ProductDetailsList/index.js
+++ src/components/ProductDetailsList/index.js
@@ -3,12 +3,12 @@ import ProductForm from '../ProductForm'
 import './styles.css';
 
 const ProductDetailsList = ({ data, addToCart, selectProduct }) => (
-  <div className='product-list-details' onClick={() => selectProduct(data)}>
-    <div className='product-list-image'>
+  <div className='product-list-details'>
+    <div className='product-list-image' onClick={() => selectProduct(data)}>
       <img alt={data.name} src={data.picture}/>
     </div>
     <div className='product-list-description'>
-      <p><strong>{data.name}</strong> - {data.company}</p>
+      <p onClick={() => selectProduct(data)}><strong>{data.name}</strong> - {data.company}</p>
       <div className='product-list-price-wrapper'>
         <div className='product-list-price'>
           <p>{data.price}</p>
